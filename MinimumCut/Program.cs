a9e1fbf1bb98d4e6cd76681be095471210672d54
Shah, Jay <jrshah0404@gmail.com>
2019-08-06 12:25:56 -0500

    Clean up code and add comments

--- MinimumCut/Program.cs
+++ MinimumCut/Program.cs
@@ -10,14 +10,20 @@ namespace MinimumCut
     {
         private static Dictionary<int, List<int>> Graph;
         private static Random _rand = new Random();
+
         /// <summary>
         /// Solution to Week 4 assignment of Coursera Algorithms specialization
+        /// 
+        /// Compute the minimum number of cuts
+        /// -------------------------------------------------------------------
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
             List<string> input = System.IO.File.ReadAllLines(args[0]).ToList();
             Graph = new Dictionary<int, List<int>>();
+            
+            // Assume input graph is valid
             foreach (string data in input)
             {
                 List<int> neighbors = data.Split('\t').ToList().Select(s => String.IsNullOrEmpty(s) ? 0 : Int32.Parse(s)).ToList();
@@ -27,32 +33,27 @@ namespace MinimumCut
                 Graph.Add(vertex, neighbors);
             }
 
-            //Graph.Add(1, new List<int>() { 2, 3 });
-            //Graph.Add(2, new List<int>() { 1, 3, 4 });
-            //Graph.Add(3, new List<int>() { 1, 2, 4 });
-            //Graph.Add(4, new List<int>() { 2, 3 });
-            // Assume graph is valid
-
-            int cuts; bool status;
-            (cuts, status) = ComputeCuts(Graph);
-
             List<int> nCuts = new List<int>();
-            int nIterations = 10000;
+            int nIterations = 1000;
 
             for (int i = 0; i < nIterations; i++)
             {
-                var x = ComputeCuts(Graph);
-                nCuts.Add(x.Item1);
+                (int, bool) x = ComputeCuts(Graph);
+                if (x.Item2)
+                    nCuts.Add(x.Item1);
             }
 
-            Console.Write(nCuts.Min());
+            Console.Write("Minimum number of cuts caluculated in {0} iterations: {1} \n", nIterations, nCuts.Min());
+            Console.Write("Probablity: {0}% \n",((double) nCuts.Count(x => x == nCuts.Min()) * 100 / nIterations));
+            Console.Write("Press any key to continue...");
             Console.Read();
         }
 
         private static (int, bool) ComputeCuts(Dictionary<int, List<int>> graph)
         {
-            // create a copy of the graph to work with
-            Dictionary<int, List<int>> _graph = graph.ToDictionary(dwItem => dwItem.Key, dwItem => dwItem.Value.ToList() );
+            // create a copy of the graph to work with since dictionaries cannot be passed by value
+            Dictionary<int, List<int>> _graph = graph.ToDictionary(dwItem => dwItem.Key, dwItem => dwItem.Value.ToList());
+
             int nVertices = _graph.Count();
             List<int> selectedList = new List<int>();
             List<int> selectedNeighbor = new List<int>();
@@ -64,9 +65,10 @@ namespace MinimumCut
                 int selectNeighbor = neighbors.ElementAt(_rand.Next(neighbors.Count()));
                 selectedList.Add(selectVertex);
                 selectedNeighbor.Add(selectNeighbor);
-                _graph = Contract(_graph, selectVertex, selectNeighbor);
+                Contract(_graph, selectVertex, selectNeighbor);
                 nVertices = _graph.Count;
             }
+
             int nCuts = _graph.First().Value.Count();
 
             foreach (var item in _graph.Values)
@@ -78,15 +80,20 @@ namespace MinimumCut
             return (nCuts, true);
         }
 
+        /// <summary>
+        /// Contraction routine
+        /// </summary>
+        /// <param name="graph"></param>
+        /// <param name="selectVertex">First vertex that will absorb the second vertex</param>
+        /// <param name="selectNeighbor">Second vertex</param>
+        /// <returns></returns>
         private static Dictionary<int, List<int>> Contract(Dictionary<int, List<int>> graph, int selectVertex, int selectNeighbor)
         {
-            Dictionary<int, List<int>> _graph = graph.ToDictionary(dwItem => dwItem.Key, dwItem => dwItem.Value.ToList());
-
-            List<int> firstVertexNeighbors = _graph[selectVertex];
-            List<int> secondVertexNeighbors = _graph[selectNeighbor];
+            List<int> firstVertexNeighbors = graph[selectVertex];
+            List<int> secondVertexNeighbors = graph[selectNeighbor];
             
             // second vertex will be replaced as neighbor by first vertex.
-            foreach (List<int> neighbors in _graph.Where(x => x.Value.Contains(selectNeighbor)).Select(item => item.Value))
+            foreach (List<int> neighbors in graph.Where(x => x.Value.Contains(selectNeighbor)).Select(item => item.Value))
             {
                 int nEdgesToReplace = neighbors.Count(x => x == selectNeighbor);
                 neighbors.RemoveAll(x => x == selectNeighbor);
@@ -96,11 +103,11 @@ namespace MinimumCut
             // neighbors of second vertex become neighbors of first vertex.
             firstVertexNeighbors = firstVertexNeighbors.Concat(secondVertexNeighbors).ToList();
             firstVertexNeighbors.RemoveAll(x => x == selectVertex || x == selectNeighbor); // remove self loops & actual selected edge
-            _graph[selectVertex] = firstVertexNeighbors;
+            graph[selectVertex] = firstVertexNeighbors;
 
-            _graph.Remove(selectNeighbor);
+            graph.Remove(selectNeighbor); // remove second vertex from the graph
 
-            return _graph;
+            return graph;
         }
     }
 }
