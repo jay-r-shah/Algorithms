64e007288cb724942e1f0ab410252bccf21f3662
Jay Shah <jrshah0404@gmail.com>
2019-08-12 20:11:49 -0500

    Add project for Course 2, week 1 assignment

---
+++ SCC/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCC
+{
+    class Program
+    {
+        /// <summary>
+        /// Solution to Week 1 programming assignment of the Graph Search, Shortest Paths, and Data Structures
+        /// course.
+        /// 
+        /// Compute the sizes of the five largest Strongly Connected Components (SCCs) of the given directed
+        /// graph.
+        /// --------------------------------------------------------------------------------------------------
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            List<string> input = System.IO.File.ReadAllLines(args[0]).ToList();
+            //Graph = new Dictionary<int, List<int>>();
+        }
+    }
+}
